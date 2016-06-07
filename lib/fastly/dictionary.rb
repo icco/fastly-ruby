6fa3199611228c43311664714492cba0049f6104
Nat Welch <nat@natwelch.com>
2016-06-07 08:50:03 -0700

    another pass

--- lib/fastly/dictionary.rb
+++ lib/fastly/dictionary.rb
@@ -11,14 +11,11 @@ class Fastly
     end
 
     def update_item(key, value)
-      begin
-        di = fetcher.get_dictionary_item(service_id, id, key)
+      di = items.select {|di| di.key.eql? key }.first
+      if di
         di.item_value = value
         fetcher.update_dictionary_item(di)
-
-      # Fastly eats the HTTP errors, really we only want to do this with 404s,
-      # but we do it for all errors because that's the data we have.
-      rescue Fastly::Error
+      else
         add_item(key, value)
       end
     end
